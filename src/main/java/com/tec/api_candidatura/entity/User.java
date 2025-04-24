8bc00c7f274a8795571cd602b6908ddccf63a26a
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-04-24 11:36:15 -0300

    create entity User

---
+++ src/main/java/com/tec/api_candidatura/entity/User.java
@@ -0,0 +1,49 @@
+package com.tec.api_candidatura.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    private UUID userid;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "created_date", updatable = false)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime createdDate;
+
+    @Column(name = "updated_date")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime updatedDate;
+    public enum Role {
+        ROLE_ADMIN,
+        ROLE_USER
+    }
+
+}
