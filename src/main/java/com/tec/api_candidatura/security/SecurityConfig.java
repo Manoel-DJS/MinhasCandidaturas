2c69dd4157fc05c3adce94f9fca6423d5ac4c3ae
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-04-28 17:01:46 -0300

    feat: add SecurityConfig class for security and Configure HttpSecurity to protect API endpoints

---
+++ src/main/java/com/tec/api_candidatura/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.tec.api_candidatura.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private static final String[] PUBLIC_MATCHERS = { "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-resource/**"};
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll() // permissão visual
+
+                        // Login
+                        .requestMatchers(HttpMethod.POST, "/api/v7/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v7/auth/login").permitAll()
+
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
