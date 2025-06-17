cf4bbcff783b21364e7fefdf2f2073293484cf3a
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-06-17 10:03:33 -0300

    problems with annotation lombok

--- src/main/java/com/tec/api_candidatura/service/impl/CandidatureServiceImpl.java
+++ src/main/java/com/tec/api_candidatura/service/impl/CandidatureServiceImpl.java
@@ -1,15 +1,25 @@
 package com.tec.api_candidatura.service.impl;
 
+import com.tec.api_candidatura.entity.Candidature;
+import com.tec.api_candidatura.repository.CandidatureRepository;
+import com.tec.api_candidatura.repository.JobVacancyRepository;
+import com.tec.api_candidatura.repository.UserRepository;
 import com.tec.api_candidatura.service.CandidatureService;
 import com.tec.api_candidatura.web.dto.request.CreateCandidatureDto;
 import com.tec.api_candidatura.web.dto.response.CandidatureResponseDto;
+import com.tec.api_candidatura.web.dto.response.JobVacancyResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CandidatureServiceImpl implements CandidatureService {
 
+    private final CandidatureRepository candidatureRepository;
+    private final UserRepository userRepository;
+    private final JobVacancyRepository jobVacancyRepository;
     @Override
     public CandidatureResponseDto apply(CreateCandidatureDto dto) {
         return null;
@@ -19,4 +29,43 @@ public class CandidatureServiceImpl implements CandidatureService {
     public List<CandidatureResponseDto> getAll() {
         return null;
     }
+
+
+
+    private CandidatureResponseDto toDto(Candidature c) {
+        return new CandidatureResponseDto(
+                c.getId(),
+                c.getClass().getName(), // vai da erro aqui
+                c.getJobVacancy().getJobTitle(),
+                c.getStatusCandidature());
+    }
+
+    /**
+     *
+     * OK
+     private CandidatureResponseDto toDto(Candidature c) {
+     CandidatureResponseDto dto = new CandidatureResponseDto();
+     dto.setId(c.getId());
+     dto.setUserName("teste");
+     dto.setJobTitle(c.getJobVacancy().getJobTitle());
+     dto.setStatus(c.getStatusCandidature());
+     return dto;
+     }
+
+     private CandidatureResponseDto toDto(Candidature c) {
+     return new CandidatureResponseDto(
+     c.getId(),
+     c.getClass().getName(),
+     c.getJobVacancy().getJobTitle(),
+     c.getStatusCandidature());
+     }
+     private CandidatureResponseDto toDto(Candidature c) {
+     return new CandidatureResponseDto(
+     c.getId(),
+     c.getJobVacancy().getUser().getUsername() ,
+     c.getJobVacancy().getJobTitle(),
+     c.getStatusCandidature());
+     }
+
+     */
 }
