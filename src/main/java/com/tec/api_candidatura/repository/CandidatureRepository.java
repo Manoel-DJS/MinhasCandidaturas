d800438439dda5d8427f732e1cff48c4aa3d765a
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-06-17 10:26:05 -0300

    fixed - add Annotation Repository

--- src/main/java/com/tec/api_candidatura/repository/CandidatureRepository.java
+++ src/main/java/com/tec/api_candidatura/repository/CandidatureRepository.java
@@ -2,7 +2,9 @@ package com.tec.api_candidatura.repository;
 
 import com.tec.api_candidatura.entity.Candidature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CandidatureRepository extends JpaRepository <Candidature, Long>{
 
 }
