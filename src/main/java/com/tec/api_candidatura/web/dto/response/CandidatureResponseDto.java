a9cee4215b899c4605da4f62f07af6a32c61eb3e
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-06-18 09:28:52 -0300

    fixed bug - lombok annotation

--- src/main/java/com/tec/api_candidatura/web/dto/response/CandidatureResponseDto.java
+++ src/main/java/com/tec/api_candidatura/web/dto/response/CandidatureResponseDto.java
@@ -4,3 +4,15 @@ import com.tec.api_candidatura.entity.enums.StatusCandidature;
 
 public record CandidatureResponseDto(Long id, String userName, String jobTitle, StatusCandidature status) {
 }
+
+/**
+
+ @Data
+ public class CandidatureResponseDto {
+ private Long id;
+ private String userName;
+ private String jobTitle;
+ private StatusCandidature status;
+ }
+
+ */
