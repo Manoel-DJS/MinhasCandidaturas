089506d8a81f55af4960110e4e4626a4953f7999
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-06-20 19:10:38 -0300

    feat: implement UserController with endpoints getAllUsers and putMapping for update data user

--- src/main/java/com/tec/api_candidatura/web/controller/UserController.java
+++ src/main/java/com/tec/api_candidatura/web/controller/UserController.java
@@ -1,17 +1,24 @@
 package com.tec.api_candidatura.web.controller;
 
 import com.tec.api_candidatura.entity.User;
+import com.tec.api_candidatura.service.UserService;
+import com.tec.api_candidatura.web.dto.request.UpdateUserDto;
+import com.tec.api_candidatura.web.dto.response.UserResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/api/v7/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/ok")
     public String ReturnOK() {
@@ -24,6 +31,18 @@ public class UserController {
         UUID userId = user.getId(); // ou getUuid(), dependendo do nome do campo
         return "User UUID: " + userId;
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> update(@PathVariable UUID id, @RequestBody UpdateUserDto dto) {
+        return ResponseEntity.ok(userService.updateUser(id, dto));
+    }
+
+    @GetMapping("getAllUsers")
+    public ResponseEntity<List<UserResponseDto>> list() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+
 
 
 }
